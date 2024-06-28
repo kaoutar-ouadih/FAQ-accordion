f0dafe4d9e7babadbccf8ebf0dfd335049b0cdcf
kaoutarOuadih1 <kaoutar.ouadih8@gmail.com>
2024-06-28 15:31:00 +0100

    a simple faq accordion using angular and taking in consideration accessibility

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,23 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'faq-accordion';
+
+
+  questions: any = [{question:"What is Frontend Mentor, and how will it help me?", answer:"Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+    isOpen: false
+  },
+    {question:"Is Frontend Mentor free?", answer:"Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.",
+      isOpen: false
+    },
+    {question:"Can I use Frontend Mentor projects in my portfolio?", answer:"Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellentway to showcase your skills to potential employers!",
+      isOpen: false
+    },
+    {question:"How can I get help if I'm stuck on a challenge?", answer:"The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
+      isOpen: false
+    }
+  ]
+
+  onQuestionClick(i: number){
+      this.questions[i].isOpen = !this.questions[i].isOpen;
+  }
 }
